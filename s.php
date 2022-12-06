d58344a22513745f4ec21c132c806f3184e77e92
Joshua <jobotron1124@hotmail.com>
2022-12-05 23:34:42 -0600

    5

---
+++ s.php
@@ -0,0 +1,32 @@
+<?php
+	
+	$conectar=@mysql_connect('localhost','root','');
+	if(!$conectar){
+		echo"No Se Pudo Conectar Con El Servidor";
+	}else{
+
+		$base=mysql_select_db('formulario');
+		if(!$base){
+			echo"No Se Encontro La Base De Datos";			
+		}
+	}
+	
+	$Titulo=$_POST['Titulo'];
+	$Imagen=$_POST['Imagen'];
+	$Descripcion=$_POST['Descripcion'];
+    $Nombredelautor=$_POST['Nombre del autor'];
+    $Fechadepublicacion=$_POST['Fecha de publicacion'];
+	$sql="INSERT INTO datos VALUES('$Titulo',
+								   '$Imagen',
+								   '$Descripcion',
+                                   '$Nombredelautor',
+                                   '$Fechadepublicacion',)";
+	
+	$ejecutar=mysql_query($sql);
+	
+	if(!$ejecutar){
+		echo"Hubo Algun Error";
+	}else{
+		echo"Datos Guardados Correctamente<br><a href='index.html'>Volver</a>";
+	}
+?>
